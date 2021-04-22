3cdd96f7fd3caf4a9b47db692b8899044fb23c85
Eduardo Kerber <Edu.a.kerber@gmail.com>
2021-04-21 21:35:25 -0300

    Splash Screen adicionada

---
+++ app/src/main/java/com/example/projetosemestralloja/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.projetosemestralloja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView logo_riter;
+    Animation animacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logo_riter = (ImageView) findViewById(R.id.ritter_logo2);
+
+        animacao = AnimationUtils.loadAnimation(this,R.anim.animacao);
+        logo_riter.setAnimation(animacao);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class) );
+
+            }
+        }, 7000);
+
+
+    }
+}
