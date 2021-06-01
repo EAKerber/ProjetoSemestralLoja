de9e385e4dece38943d96f5f7538c6384f850abe
Eduardo Kerber <Edu.a.kerber@gmail.com>
2021-05-31 22:15:56 -0300

    adicionada nova pagina principal do carrinho PaginaCarrinho

---
+++ app/src/main/java/com/example/projetosemestralloja/PaginaCarrinho.java
@@ -0,0 +1,14 @@
+package com.example.projetosemestralloja;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaginaCarrinho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_carrinho);
+    }
+}
