37456375deca1c16b0d0879f237a5e518ec66adc
Eduardo Kerber <Edu.a.kerber@gmail.com>
2021-06-01 18:28:02 -0300

    HomeFragment funcionalidade desativada

--- app/src/main/java/com/example/projetosemestralloja/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/projetosemestralloja/ui/home/HomeFragment.java
@@ -30,7 +30,7 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
-                if(!s.toLowerCase().equals("home"))
+                if(false)
                     startActivity(new Intent(getContext(), MainActivity2.class));
             }
         });
