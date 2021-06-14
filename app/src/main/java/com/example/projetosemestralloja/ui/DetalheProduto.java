94bca3c437b6da7339a5c8a716c0da98ac3d6ebc
Eduardo Kerber <Edu.a.kerber@gmail.com>
2021-06-13 22:38:28 -0300

    inicio parte compartilhar

--- app/src/main/java/com/example/projetosemestralloja/ui/DetalheProduto.java
+++ app/src/main/java/com/example/projetosemestralloja/ui/DetalheProduto.java
@@ -9,10 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.BindingAdapter;
 import androidx.databinding.DataBindingUtil;
 
-import com.example.projetosemestralloja.model.Produto;
 import com.example.projetosemestralloja.R;
 import com.example.projetosemestralloja.databinding.ActivityDetalheProdutoBinding;
+import com.example.projetosemestralloja.model.Produto;
 import com.squareup.picasso.Picasso;
+
 
 public class DetalheProduto extends AppCompatActivity {
 
@@ -31,6 +32,37 @@ public class DetalheProduto extends AppCompatActivity {
 
     public void addAoCarrinho(View v){
         pg.createItemDoCarrinho(produtoDetalhe, v);
+    }
+
+    public void compartilhar(View v){/*
+        ImageView imageView = findViewById(R.id.imageView6);
+        Intent intent = new  Intent(Intent.ACTION_SEND);
+        intent.setType("image/jpeg");
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        Log.d("compratilhar", "01");
+
+
+        String packageName = getPackageName();
+        getApplicationContext().grantUriPermission(packageName,  Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Produto", null)),
+            Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        Uri uri = Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Produto", null));
+
+
+
+        Log.d("compratilhar", "02");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(intent,  "produto"));*/
+
+        Intent intent = new  Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        String sub = "http://www.example.com/gizmos";
+        intent.putExtra(Intent.EXTRA_TEXT, sub);
+        startActivity(Intent.createChooser(intent,  "produto"));
+
     }
 
     @BindingAdapter({"imageUrl2"})
---
+++ app/src/main/java/com/example/projetosemestralloja/ui/FinalizarCompra.java
@@ -0,0 +1,16 @@
+package com.example.projetosemestralloja.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projetosemestralloja.R;
+
+public class FinalizarCompra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalizar_compra);
+    }
+}
